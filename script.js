a60f5406767befe5f732a5af0f608ad8507721c3
Sarkorbek Abdullaev <sarkorbek7777@gmail.com>
2022-06-22 09:31:09 +0500

    javascript basics

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log('Hello Web Dev School');
+document.write('Hello');
+alert('salom');
+
+/* VAR */
+
+var WebDevSchool = 'Sarkorbek';
+console.log(WebDevSchool);
+
+var name = 'block-1';
+var name = 'block-2';
+var name = 'block-3';
+var name = 'block-4';
+var name = 'block-5';
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+console.log(name);
+
+
+var age = 20;
+var name = 'Sarkorbek';
+
+console.log('Mening ismim ' + name + ', yoshim ' + age + 'da.');
+
+
+/* ---------------------OPERATORLAR-------------------- */
+
+var num1 = 10;
+var num2 = 4;
+console.log(num1 % num2);
+
+
+var selectedColor = null;
+selectedColor = 'blue';
+selectedColor = null;
+console.log(selectedColor);
